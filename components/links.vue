b681d1b94434b1c87d48ac0158309a80e47e98e4
2lay <mail@2lay.net>
2024-01-29 02:21:01 +0100

    forgot to add pronouns.page link :s

--- components/links.vue
+++ components/links.vue
@@ -3,6 +3,10 @@
         style="background-color: rgba(123, 148, 238, 0.212); border: 1px solid #4e5776;"><span>
         </span><font-awesome-icon icon="fa-brands fa-discord" /> &nbsp;Discord (2lay)</v-btn>
 
+    <v-btn block href="https://en.pronouns.page/@2lay" class="text-center custom-btn spacing"
+        style="background-color: rgba(238, 123, 226, 0.212); border: 1px solid #764e6b;"><span>
+        </span><span class="mdi mdi-card-account-details" /> &nbsp;Pronouns.page</v-btn>
+
     <v-btn block href="https://github.com/2lay" class="text-center custom-btn spacing"
         style="background-color: rgba(147, 147, 147, 0.212); border: 1px solid #555555;"><span>
         </span><font-awesome-icon icon="fa-brands fa-github" /> &nbsp;GitHub</v-btn>
@@ -17,7 +21,7 @@
 
     <v-btn block href="https://pcpartpicker.com/user/2lay/saved/cQv8ZL" class="text-center custom-btn spacing"
         style="background-color: rgba(255, 227, 149, 0.212); border: 1px solid #5a4b24;"><span>
-        </span><span class="mdi mdi-laptop"></span>&nbsp;My setup</v-btn>
+        </span><span class="mdi mdi-laptop" />&nbsp;My setup</v-btn>
 
     <v-btn block href="https://www.youtube.com/@2lay" class="text-center custom-btn spacing"
         style="background-color: rgba(255, 0, 0, 0.212); border: 1px solid #5a2424;"><span>
