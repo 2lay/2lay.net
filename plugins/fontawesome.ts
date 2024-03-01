5e74b20f4fc90293fab19059271e920ec98e986c
ashley <mail@2lay.net>
2024-03-01 02:13:17 +0100

    removed: unused icon

--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -1,8 +1,8 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faDiscord, faGithub, faLastfm, faYoutube, faSteam, faMastodon } from '@fortawesome/free-brands-svg-icons'
+import { faDiscord, faGithub, faLastfm, faYoutube, faSteam } from '@fortawesome/free-brands-svg-icons'
 
-library.add(faDiscord, faGithub, faLastfm, faYoutube, faSteam, faMastodon)
+library.add(faDiscord, faGithub, faLastfm, faYoutube, faSteam,)
 config.autoAddCss = false
 
 export default defineNuxtPlugin((nuxtApp) => {
