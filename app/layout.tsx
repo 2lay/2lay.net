248d1b18551e28ca043db9b0b2a56d0d29c7e0aa
2lay <mail@2lay.net>
2024-07-23 05:43:47 +0100

    feat: footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
             <head>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </head>
-            <body className={`${font.className} bg-ash_background-500 pt-10 pb-5 px-6 max-w-screen-lg mx-auto`}>
+            <body className={`${font.className} bg-ash_background-500 max-w-screen-lg mx-auto`}>
                 {children}
             </body>
         </html>
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-        <main>
-            <div className="text-ash_primary-400 flex justify-between border-ash_primary-400 border-2 rounded-xl px-3 py-2 mb-10">
+        <main className="flex flex-col min-h-screen justify-between">
+            {/* nav */}
+            <div className="text-ash_primary-400 flex justify-between border-ash_primary-400 border-2 rounded-xl px-3 py-2 mb-10 mt-5">
                 <div className="flex">
                     <h1 className="font-extrabold">2lay.net</h1>
                     <h1 className="mx-1"> |</h1>
@@ -15,7 +16,9 @@ export default function Home() {
                     <p>[ Home ]</p>
                 </div>
             </div>
-            <div className="text-ash_primary-400 px-5">
+
+            {/* content */}
+            <div className="text-ash_primary-400 px-5 flex-grow">
                 <div>
                     <div className="flex items-center mb-5">
                         <img src="https://github.com/2lay.png" className="max-w-24 rounded-3xl mr-4" />
@@ -34,6 +37,14 @@ export default function Home() {
                         My favorite interests are Linux and open-source software. Besides that, I love immersing myself
                         in music and video games <a className="font-bold">^w^</a>
                     </p>
+                </div>
+            </div>
+
+            {/* footer */}
+            <div className="border-t-2 border-ash_primary-400 text-ash_primary-400 mt-4 px-1">
+                <div className="flex  justify-between mt-1 mb-2 text-sm font-thin ">
+                    <div className="text-center  ">2021-2024 2lay.net</div>
+                    <div className="text-center ">read if cute</div>
                 </div>
             </div>
         </main>
