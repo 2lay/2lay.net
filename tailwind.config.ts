f79b071c2e1b0c767e25ffdbdf45e0972c67b661
2lay <mail@2lay.net>
2024-08-03 00:45:21 +0100

    rem: animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -44,21 +44,6 @@ const config: Config = {
             },
         },
     },
-    
-    keyframes: {
-        fadeIn: {
-            "0%": { opacity: 0 },
-            "100%": { opacity: 1 },
-        },
-        fadeOut: {
-            "0%": { opacity: 1 },
-            "100%": { opacity: 0 },
-        },
-    },
-    animation: {
-        fadeIn: "fadeIn 0.5s ease-in-out",
-        fadeOut: "fadeOut 0.5s ease-in-out",
-    },
 },
   plugins: [],
 };
