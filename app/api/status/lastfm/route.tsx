55e4399254436d52cf6e48f9b34d52b0253d8d43
2lay <mail@2lay.net>
2024-08-04 03:59:16 +0100

    fix: interface api

--- app/api/status/lastfm/route.tsx
+++ app/api/status/lastfm/route.tsx
@@ -1,19 +1,32 @@
 import { NextResponse } from "next/server";
 
+interface TrackImage {
+    size: string;
+    "#text": string;
+}
+
+interface RecentTrack {
+    name: string;
+    album: { "#text": string };
+    artist: { "#text": string };
+    image: TrackImage[];
+    date: { uts: string };
+}
+
 export const GET = async () => {
-    const apikey = process.env.LFM_KEY
-    console.log(apikey)
+    const apikey = process.env.LFM_KEY;
+    console.log(apikey);
     const res = await fetch(
         `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=twolay&limit=1&api_key=${apikey}&format=json`
     );
     const repo = await res.json();
-    const req = repo.recenttracks.track[0];
+    const req: RecentTrack = repo.recenttracks.track[0];
 
     const track = req.name;
     const album = req.album["#text"];
     const artist = req.artist["#text"];
-    const image = req.image.find((img) => img.size === "large")["#text"];
-    const date = req.date["uts"];
+    const image = req.image.find((img: TrackImage) => img.size === "large")?.["#text"];
+    const date = req.date.uts;
 
     const response = {
         track,
