40381484e900a635f58b997897e537d8660f5a11
2lay <mail@2lay.net>
2024-10-05 10:15:50 +0200

    fix: 404 colours

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,17 +8,17 @@ export const metadata: Metadata = {
 
 export default function Home() {
     return (
-        <main className="flex items-center justify-center min-h-[50vh]">
+        <main className="flex items-center justify-center">
             <div className="text-neutral-100 text-center p-6">
                 <div className="flex flex-col gap-y-4 items-center">
                     <div>
-                        <p className="text-6xl text-purple-500 animate-bounce">{">w<"}</p>
+                        <p className="text-6xl text-pink-300 animate-bounce">{">w<"}</p>
                         <p className="text-2xl font-semibold text-gray-300">uh oh, this page is not here</p>
                     </div>
 
                     <Link
                         href="/"
-                        className="flex justify-center items-center space-x-2 px-5 py-2 rounded-lg text-purple-500 transition-colors transform hover:text-purple-400 font-semibold">
+                        className="flex justify-center items-center space-x-2 px-5 py-2 rounded-lg text-pink-300 transition-colors transform hover:text-pink-400 font-semibold">
                         Go Home
                     </Link>
                 </div>
