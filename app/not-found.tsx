0bb3d8cf9738f1b188463e05a7cbff739793978b
2lay <mail@2lay.net>
2024-10-05 10:19:37 +0200

    feat: 404 butto

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -15,11 +15,11 @@ export default function Home() {
                         <p className="text-6xl text-pink-300 animate-bounce">{">w<"}</p>
                         <p className="text-2xl font-semibold text-gray-300">uh oh, this page is not here</p>
                     </div>
-
                     <Link
                         href="/"
-                        className="flex justify-center items-center space-x-2 px-5 py-2 rounded-lg text-pink-300 transition-colors transform hover:text-pink-400 font-semibold">
-                        Go Home
+                        className="flex justify-between items-center space-x-2 px-4 py-2 transition-all duration-300 ease-in-out bg-[#f9a8d433] hover:bg-[#f9a8d44D] text-[#f9a8d4] hover:shadow-[5px_5px_0px_0px_#f472b6]"
+                    >
+                        <span>Go Home</span>
                     </Link>
                 </div>
             </div>
