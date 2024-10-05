531db0598a244e9e99d0a5ac79c8c3e227ac6e14
2lay <mail@2lay.net>
2024-10-05 10:33:33 +0200

    feat: better projects card

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -17,7 +17,7 @@ export default function Projects() {
             <div>
                 <div className="flex flex-col gap-y-2 mb-5">
                     <p className="text-3xl">
-                        <a className="font-bold">$</a> my projects
+                        <span className="font-bold">$</span> my projects
                     </p>
                     <p>
                         welcome to my projects page! here you&#39;ll find a collection of my recent work and
@@ -25,39 +25,25 @@ export default function Projects() {
                     </p>
                 </div>
                 <div className="flex flex-col max-w-lg">
-                    <div className="border border-neutral-800 max-w-max p-6 shadow-lg bg-neutral-900 text-white rounded-lg">
+                    <div className="border border-neutral-800 p-6 bg-neutral-900 text-white rounded-lg transition-all duration-300 ease-in-out hover:shadow-[5px_5px_0px_0px_#f472b6]">
                         <a
                             href="https://tmw.gg/"
-                            className="text-2xl font-bold hover:text-gray-400 transition-colors duration-300 block mb-2">
+                            className="text-2xl font-bold hover:text-pink-300 transition-colors duration-300 block mb-2">
                             TMW.gg
                         </a>
-                        <p className="text-sm text-gray-400 mb-4">Founder & DevOps | 2022 - now</p>
-                        <p className="text-xs text-gray-300 mb-4">
+                        <p className="text-sm text-neutral-400 mb-4">Founder & DevOps | 2022 - now</p>
+                        <p className="text-sm text-neutral-300 mb-4">
                             Experience: Paper Plugin Development, NMS, Web Development, Mixins, Infrastructure Management
                         </p>
-                        <div className="text-xs text-gray-500">
+                        <div className="text-xs">
                             <div className="flex gap-2 flex-wrap">
-                                <span className="text-purple-400 bg-purple-800 bg-opacity-30 px-2 py-1 rounded">
-                                    Kotlin
-                                </span>
-                                <span className="text-yellow-300 bg-yellow-600 bg-opacity-30 px-2 py-1 rounded">
-                                    Java
-                                </span>
-                                <span className="text-blue-300 bg-blue-600 bg-opacity-30 px-2 py-1 rounded">
-                                    TypeScript
-                                </span>
-                                <span className="text-gray-300 bg-gray-600 bg-opacity-30 px-2 py-1 rounded">
-                                    Next.js
-                                </span>
-                                <span className="text-teal-300 bg-teal-600 bg-opacity-30 px-2 py-1 rounded">
-                                    MySQL
-                                </span>
-                                <span className="text-blue-400 bg-blue-800 bg-opacity-30 px-2 py-1 rounded">
-                                    Docker
-                                </span>
-                                <span className="text-green-300 bg-green-600 bg-opacity-30 px-2 py-1 rounded">
-                                    CI/CD
-                                </span>
+                                <span className="bg-neutral-800 px-2 py-1 rounded text-pink-300">Kotlin</span>
+                                <span className="bg-neutral-800 px-2 py-1 rounded text-yellow-300">Java</span>
+                                <span className="bg-neutral-800 px-2 py-1 rounded text-blue-300">TypeScript</span>
+                                <span className="bg-neutral-800 px-2 py-1 rounded text-neutral-300">Next.js</span>
+                                <span className="bg-neutral-800 px-2 py-1 rounded text-teal-300">MySQL</span>
+                                <span className="bg-neutral-800 px-2 py-1 rounded text-blue-400">Docker</span>
+                                <span className="bg-neutral-800 px-2 py-1 rounded text-green-300">CI/CD</span>
                             </div>
                         </div>
                     </div>
