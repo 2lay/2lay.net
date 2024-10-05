093c2b3cf2c7d31f0e4db3a825f7387f6ef01264
2lay <mail@2lay.net>
2024-10-05 11:51:16 +0200

    fix: scrolling

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,26 +21,28 @@ export default function RootLayout({
                 <link rel="icon" href="https://github.com/2lay.png" sizes="any" />
             </head>
 
-            <body className={`${font.className} bg-[#111111] min-h-screen relative max-w-screen-lg mx-auto mt-8 text-lg`}>
+            <body className={`${font.className} bg-[#111111] min-h-screen relative max-w-screen-lg mx-auto  text-lg`}>
                 {/* Noise background, credits to https://arc.net/ */}
                 <div className="fixed inset-0 bg-[url('/noise.png')] bg-repeat opacity-30 pointer-events-none z-[-1]"></div>
 
-                {/* Navbar */}
-                <div className="flex justify-between items-center bg-neutral-950 rounded-t-lg px-3 py-2 mx-4 md:mx-6 lg:mx-8 xl:mx-12 2xl:mx-16">
-                    <div className="text-pink-300 hidden md:block">
-                        <a>&gt; 2lay.net</a>
+                <div className="pt-6">
+                    {/* Navbar */}
+                    <div className="flex justify-between items-center bg-neutral-950 rounded-t-lg px-3 py-2 mx-4 md:mx-6 lg:mx-8 xl:mx-12 2xl:mx-16">
+                        <div className="text-pink-300 hidden md:block">
+                            <a>&gt; 2lay.net</a>
+                        </div>
+                        <Nav />
                     </div>
-                    <Nav />
-                </div>
 
-                {/* Main Content */}
-                <div className="bg-[#0f0f0f] text-neutral-200 px-3 mx-4 md:mx-6 lg:mx-8 xl:mx-12 2xl:mx-16 py-4">
-                    <div className="py-[0.01px]">{children}</div>
-                </div>
+                    {/* Main Content */}
+                    <div className="bg-[#0f0f0f] text-neutral-200 px-3 mx-4 md:mx-6 lg:mx-8 xl:mx-12 2xl:mx-16 py-4">
+                        <div className="py-[0.01px]">{children}</div>
+                    </div>
 
-                {/* Footer */}
-                <div className="bg-neutral-950 rounded-b-lg px-3 py-2 mx-4 md:mx-6 lg:mx-8 xl:mx-12 2xl:mx-16">
-                    <a className="text-neutral-500">2lay.net © 2024</a>
+                    {/* Footer */}
+                    <div className="bg-neutral-950 rounded-b-lg px-3 py-2 mx-4 md:mx-6 lg:mx-8 xl:mx-12 2xl:mx-16">
+                        <a className="text-neutral-500">2lay.net © 2024</a>
+                    </div>
                 </div>
             </body>
         </html>
