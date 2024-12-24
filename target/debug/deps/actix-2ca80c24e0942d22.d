0cf1cd58c7267cc978f66e02ec2178de2faaf55a
2lay <mail@2lay.net>
2024-12-24 01:09:35 +0100

    feat: init

---
+++ target/debug/deps/actix-2ca80c24e0942d22.d
@@ -0,0 +1,5 @@
+C:\Users\ellie\Desktop\actix\target\debug\deps\libactix-2ca80c24e0942d22.rmeta: src/main.rs
+
+C:\Users\ellie\Desktop\actix\target\debug\deps\actix-2ca80c24e0942d22.d: src/main.rs
+
+src/main.rs:
