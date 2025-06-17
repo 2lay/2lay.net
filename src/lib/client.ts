c5f72437336cb18a1b62c5878f47c33bc28caa4a
2lay <mail@2lay.net>
2025-06-17 13:48:27 +0200

    feat: vercel

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,10 +1,21 @@
 import type { AppRouter } from "@/server"
 import { createClient } from "jstack"
 
-/**
- * Your type-safe API client
- * @see https://jstack.app/docs/backend/api-client
- */
 export const client = createClient<AppRouter>({
-  baseUrl: "https://2lay.net/api",
+  baseUrl: `${getBaseUrl()}/api`,
 })
+
+function getBaseUrl() {
+  // 👇 Use browser URL if client-side
+  if (typeof window !== "undefined") {
+    return window.location.origin
+  }
+
+  // 👇 Use Vercel URL in production
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`
+  }
+
+  // 👇 Default to localhost
+  return `http://localhost:3000`
+}
