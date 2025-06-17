846d8711b7ff196b1077a925f03b77a7e6c4311c
2lay <mail@2lay.net>
2025-06-17 13:43:55 +0200

    feat: endpoint url

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -6,5 +6,5 @@ import { createClient } from "jstack"
  * @see https://jstack.app/docs/backend/api-client
  */
 export const client = createClient<AppRouter>({
-  baseUrl: "http://localhost:3000/api",
+  baseUrl: "https://2lay.net/api",
 })
